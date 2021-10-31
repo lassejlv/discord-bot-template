cd3dd3e7a448b90b931ffc5fc74465fce9a0b26e
lassev <77295879+lassev05@users.noreply.github.com>
2021-10-31 18:33:23 +0100

    add alliases
    alliases are added!

--- events/guild/message.js
+++ events/guild/message.js
@@ -8,7 +8,9 @@ module.exports = (Discord, client, message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const cmd = args.shift().toLowerCase();
 
-  const command = client.commands.get(cmd);
-
+   const command =
+    client.commands.get(cmd) ||
+    client.commands.find((a) => a.alliases && a.alliases.includes(cmd));
+  
   if (command) command.execute(client, message, args, Discord);
 };
