14fce86c7e56cf3fd33a61e78c1e8fdcf5a20bdf
devlassev <vestergaardlasse2@gmail.com>
2021-10-31 16:51:21 +0100

    ddd

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "this is a ping command!",
-  execute(message, args) {
-    const mention = message.author.id;
-
-    message.channel.send(`<@${mention}> hello!`);
-  },
-};
